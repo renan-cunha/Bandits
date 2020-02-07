f3f075270f3ca5a9e98455e57fa6b6f67c8b9162
renan <renancunhafonseca@gmail.com>
2020-02-07 06:33:53 -0300

    Rename bandit.py to testbed.py

---
+++ src/testbed.py
@@ -0,0 +1,25 @@
+from typing import List
+from src.arm import Arm
+import random
+
+
+class Testbed:
+
+    def __init__(self, arms: List[Arm]):
+        self.arms = arms
+    
+    def act(self, action: int):
+        if action < 0 or action >= self.get_num_actions():
+            raise ValueError(f"Action {action} should be on the interval "
+                             f"[0, {self.get_num_actions()-1}]")
+        return self.arms[action].pull()
+
+    def get_num_actions(self) -> int:
+        return len(self.arms)
+
+    def random_walk(self, mean: float, stddev: float) -> None:
+        for arm in self.arms:
+            arm.random_walk(mean, stddev)
+
+    def random_action(self) -> int:
+        return random.randint(0, self.get_num_actions()-1)
