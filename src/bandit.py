8312257cba641ff541bb179b920f4c87153e6c5d
renan <renancunhafonseca@gmail.com>
2020-02-08 16:47:07 -0300

    Add Upper-Confidence bount option on bandit

--- src/bandit.py
+++ src/bandit.py
@@ -13,17 +13,24 @@ class Bandit:
         self.step_size = 0.1
         self.action_counts = np.zeros(testbed.get_num_actions())
         self.sample_average = False
+        self.ucb = False
+        self.c = 0.0
     
-    def run(self, num_steps: int, epsilon: float, sample_average: bool,
+    def run(self, num_steps: int, epsilon: float = 0, 
+            sample_average: bool = False,
             step_size: float = 0.1,
             random_walk_stddev: float = 0.0,
-            initial_estimate: float = 0.0) -> Tuple[np.ndarray, np.ndarray]:
+            initial_estimate: float = 0.0,
+            ucb: bool = False,
+            c: float = 0.0) -> Tuple[np.ndarray, np.ndarray]:
         """Returns a tuple containing:
             1) an array with rewards per step
             2) a boolean array inidicating if the action is optimal per step"""
         self.action_counts = np.zeros(self.testbed.get_num_actions())
         self.sample_average = sample_average
         self.step_size = step_size
+        self.ucb = ucb
+        self.c = c
 
         testbed = deepcopy(self.testbed)
         num_actions = testbed.get_num_actions()
@@ -35,7 +42,7 @@ class Bandit:
 
         for step in range(num_steps):
             
-            action = self.action_policy(epsilon)
+            action = self.action_policy(epsilon, step)
             reward = testbed.act(action)
             rewards[step] = reward
             is_optimal[step] = (action == testbed.optimal_action())
@@ -44,14 +51,22 @@ class Bandit:
                 testbed.random_walk(0, random_walk_stddev)
         return rewards, is_optimal
 
-    def action_policy(self, epsilon: float) -> int:
-        coin = random.uniform(0, 1)
-        if coin > epsilon:
-            max_value = np.max(self.action_estimates)
-            max_actions = np.where(max_value == self.action_estimates)[0]
-            action = random.choice(max_actions)
+    def action_policy(self, epsilon: float, step: int) -> int:
+        
+        if self.ucb:
+            step += 1
+            action_counts = self.action_counts + 10**-20  # prevent overflow
+            ucb_exploration = np.sqrt(np.log(step)/action_counts)
+            bound_estimation = self.action_estimates + self.c * ucb_exploration
+            action = np.argmax(bound_estimation)
         else:
-            action = self.testbed.random_action()
+            coin = random.uniform(0, 1)
+            if coin > epsilon:
+                max_value = np.max(self.action_estimates)
+                max_actions = np.where(max_value == self.action_estimates)[0]
+                action = random.choice(max_actions)
+            else:
+                action = self.testbed.random_action()
         self.action_counts[action] += 1
         return action
 
