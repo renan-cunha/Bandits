Fix exercise-2-5 with correct sample averages implementation

exercise-2-5.py
@@ -14,15 +14,6 @@ epsilon = 0.1
 step_size = 0.1
 random_walk_stddev = 0.01
 
-
-def sample_average_function(step: int) -> float:
-    return 1.0/(step+1)
-
-
-def parameter_function(step: int) -> float:
-    return step_size
-
-
 rewards = np.zeros((num_runs, num_steps, 2))
 is_optimal = np.zeros((num_runs, num_steps, 2), dtype=bool)
 
@@ -30,13 +21,17 @@ for run in tqdm(range(num_runs)):
     unit_arms = [Arm(mean, stddev) for x in range(num_arms)]
     testbed = Testbed(unit_arms)
     bandit = Bandit(testbed)
-    for function_index, step_function in enumerate([sample_average_function,
-                                                    parameter_function]):
-        run_rewards, run_is_optimal = bandit.run(num_steps, epsilon, 
-                                                 step_function, 
-                                                 random_walk_stddev)
-        rewards[run, :, function_index] = run_rewards
-        is_optimal[run, :, function_index] = run_is_optimal
+    sample_rewards, sample_optimal = bandit.run(num_steps, epsilon, 
+                                                sample_average=True,
+                                                random_walk_stddev=random_walk_stddev)
+    rewards[run, :, 0] = sample_rewards
+    is_optimal[run, :, 0] = sample_optimal
+    step_size_rewards, step_size_optimal = bandit.run(num_steps, epsilon, 
+                                                      step_size=step_size,
+                                                      sample_average=False,
+                                                      random_walk_stddev=random_walk_stddev)
+    rewards[run, :, 1] = step_size_rewards
+    is_optimal[run, :, 1] = step_size_optimal
 
 np.save("data/rewards.npy", rewards)
 np.save("data/is_optimal.npy", is_optimal)